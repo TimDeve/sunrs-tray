29d0e907fa92147f997cc834081e097a3b10e261
Tim Deve <timdeve@users.noreply.github.com>
2022-01-11 11:56:10 +0000

    Allows to control more than one bulb at a time
    Using config file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #![windows_subsystem = "windows"]
 
 use std::env;
-use sunrs::messages::{SetColorMessage, SetPowerMessage};
+use sunrs::messages::{Command, SetColorMessage, SetPowerMessage};
 use sunrs::net::send_message;
-use sunrs::scenes::{BRIGHT, CHILL, COMPUTER, DARK, DAYLIGHT, READING};
 
 fn ok() -> Result<(), systray::Error> {
     Ok::<_, systray::Error>(())
@@ -11,75 +10,70 @@ fn ok() -> Result<(), systray::Error> {
 
 #[cfg(target_os = "windows")]
 fn main() -> Result<(), systray::Error> {
-    match systray::Application::new() {
-        Err(_) => panic!("Can't create window!"),
-        Ok(mut app) => {
-            let local_app_data = env::var("LOCALAPPDATA").expect("Can't get %LOCALAPPDATA%");
+    let mut app = systray::Application::new().expect("Can't create window!");
 
-            app.set_icon_from_file(&format!(
-                r"{}\Programs\sunrs-tray\sunrs-tray.ico",
-                local_app_data
-            ))?;
+    let local_app_data = env::var("LOCALAPPDATA").expect("Can't get %LOCALAPPDATA%");
 
-            app.set_tooltip("Sunrs")?;
+    app.set_icon_from_file(&format!(
+        r"{}\Programs\sunrs-tray\sunrs-tray.ico",
+        local_app_data
+    ))?;
 
-            app.add_menu_item("On", |_| {
-                send_message(SetPowerMessage::new_on_message());
-                ok()
-            })?;
+    let config = sunrs::config::Config::from_default_file();
 
-            app.add_menu_item("Off", |_| {
-                send_message(SetPowerMessage::new_off_message());
-                ok()
-            })?;
+    app.set_tooltip("Sunrs")?;
 
-            app.add_menu_separator()?;
+    let config_clone = config.clone();
+    app.add_menu_item("On", move |_| {
+        send_on_message(config_clone.bulbs_addresses());
+        ok()
+    })?;
 
-            app.add_menu_item("Daylight", |_| {
-                send_message(SetColorMessage::new_scene(DAYLIGHT));
-                send_message(SetPowerMessage::new_on_message());
-                ok()
-            })?;
+    let config_clone = config.clone();
+    app.add_menu_item("Off", move |_| {
+        send_off_message(config_clone.bulbs_addresses());
+        ok()
+    })?;
 
-            app.add_menu_item("Warm Bright", |_| {
-                send_message(SetColorMessage::new_scene(BRIGHT));
-                send_message(SetPowerMessage::new_on_message());
-                ok()
-            })?;
+    app.add_menu_separator()?;
 
-            app.add_menu_item("Computer", |_| {
-                send_message(SetColorMessage::new_scene(COMPUTER));
-                send_message(SetPowerMessage::new_on_message());
-                ok()
-            })?;
-
-            app.add_menu_item("Reading", |_| {
-                send_message(SetColorMessage::new_scene(READING));
-                send_message(SetPowerMessage::new_on_message());
-                ok()
-            })?;
-
-            app.add_menu_item("Chill", |_| {
-                send_message(SetColorMessage::new_scene(CHILL));
-                send_message(SetPowerMessage::new_on_message());
-                ok()
-            })?;
-
-            app.add_menu_item("Dark", |_| {
-                send_message(SetColorMessage::new_scene(DARK));
-                send_message(SetPowerMessage::new_on_message());
-                ok()
-            })?;
-
-            app.add_menu_separator()?;
-
-            app.add_menu_item("Quit", |window| {
-                window.quit();
-                ok()
-            })?;
-
-            app.wait_for_message()?;
+    for (i, scene) in config.scenes.iter().enumerate() {
+        let config_clone = config.clone();
+        app.add_menu_item(&scene.name, move |_| {
+            let commands = config_clone
+                .scene_by_index(i)
+                .expect("Should have matching scene as index pulled from iter");
+            send_scene_messages(commands);
             ok()
-        }
+        })?;
+    }
+
+    app.add_menu_separator()?;
+
+    app.add_menu_item("Quit", |window| {
+        window.quit();
+        ok()
+    })?;
+
+    app.wait_for_message()?;
+    ok()
+}
+
+fn send_on_message(addresses: Vec<[u8; 8usize]>) {
+    for address in addresses.iter() {
+        send_message(SetPowerMessage::new_on_message(*address))
+    }
+}
+
+fn send_off_message(addresses: Vec<[u8; 8usize]>) {
+    for address in addresses.iter() {
+        send_message(SetPowerMessage::new_off_message(*address))
+    }
+}
+
+fn send_scene_messages(scene: Vec<Command>) {
+    for command in scene.iter() {
+        send_message(SetColorMessage::new_scene(*command));
+        send_message(SetPowerMessage::new_on_message(command.mac_address))
     }
 }
